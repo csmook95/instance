a861e2e318191cc5520b69b0287f26a39a446cbc
csmook95 <csmook95@gmail.com>
2023-03-09 01:06:37 +0900

    2023.03.09

--- testjs/main.js
+++ testjs/main.js
@@ -1,5 +1,5 @@
-const inputA = document.querySelector(`#inputA`)
-console.log(inputA)
-inputA.addEventListener(`focusout`, () => {
-    console.log(`focusout`)
+const aa = document.querySelector(`#aa`)
+aa.addEventListener(`click`, () => {
+    fetch(`https://localhost:44342/Home/FileDownload`)
+        .then(res => { console.log(res) })
 })
